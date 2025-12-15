9372069a9b51b188ba9050edc4679fa475a621f6
Sameer Naeem <sameernm2003@outlook.com>
2025-12-15 18:10:45 +0000

    FastAPI setup

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return{"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
