06413635e798008d3e62733936ba53f9fa564b57
Sameer Naeem <sameernm2003@outlook.com>
2026-01-16 21:22:12 +0000

    Implemented ai plan generator logic

--- backend/app/routes/workouts.py
+++ backend/app/routes/workouts.py
@@ -1,20 +1,35 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-
+from backend.app.routes.auth import get_current_user, db_dependency
+from backend.app.services.workout_service import generate_and_store_plan
+from backend.app.schemas import WorkoutRead
+from backend.app.models import Workout
 from backend.app.database import get_db
 
 router = APIRouter(prefix="/workouts", tags=["workouts"])
 
 
-@router.get("/")
-def get_workouts():
-    pass
+@router.get("/", response_model=list[WorkoutRead])
+def get_workouts(db: db_dependency, current_user = Depends(get_current_user)):
+    workouts = db.query(Workout).filter(Workout.user_id == current_user.id).all()
+    if not workouts:
+        raise HTTPException(status_code=404, detail="No workouts found")
+    return workouts
 
 
-@router.get("/{id}")
-def get_workout_by_id():
-    pass
+@router.get("/{id}", response_model=WorkoutRead)
+def get_workout_by_id(id: int, db: db_dependency, current_user = Depends(get_current_user)):
+    workout = db.query(Workout).filter(Workout.id == id).first()
+    if not workout:
+        raise HTTPException(status_code=404, detail="Workout not found")
+    if workout.user_id != current_user.id:
+        raise HTTPException(status_code=403, detail="Workout does not belong to current user")
+    return workout
 
 
 @router.post("/generate")
-async def generate_workouts():
-    pass
+async def generate_workouts(db: db_dependency, current_user = Depends(get_current_user)):
+    try:
+        plan = generate_and_store_plan(db, current_user)
+        return {"message": "Workout plan created", "plan": plan}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- backend/app/services/plan_generator.py
+++ backend/app/services/plan_generator.py
@@ -1,2 +1,73 @@
-async def generate_plan():
-    pass
+from openai import OpenAI
+
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+env_path = BASE_DIR / ".env"
+load_dotenv(env_path)
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=openai_api_key)
+
+def generate_workout_plan(prompt):
+    system_prompt = "You are a helpful assistant designed to output JSON. Do not include any markdown backticks in your response."
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[{"role": "system", "content": system_prompt}, {"role": "user", "content": prompt}],
+        seed=42,
+        response_format={"type": "json_object"}
+    )
+    return response.choices[0].message.content
+
+
+def build_prompt(profile, exercises):
+    return f"""
+    Create an effective 4 week workout plan specifically tailored for the user profile below. Your answer must be strictly JSON.
+
+    User Profile:
+    Goal: {profile.goal}
+    Experience Level: {profile.experience_level}
+    Weekly Frequency: {profile.frequency}
+    Equipment Available: {[e.name for e in profile.equipment]}
+    Injuries Sustained: {[i.name for i in profile.injuries]}
+
+    Available exercises (Only choose from these and make sure they are suitable for the user given their profile details, try to pick more conventional exercises that the user will be able to easily understand):
+    {[
+        {
+            "id": e.name,
+            "name": e.name,
+            "muscle": e.target_muscle,
+            "equipment": e.equipment.name if e.equipment else None
+        }
+        for e in exercises
+    ]}
+
+    Format the ouput in JSON so it has the following structure:
+    {{
+        "weeks": [
+        {{
+            "week_number": 1,
+            "days": [
+            {{
+                "day_number": 1,
+                "date_offset": 0,
+                "exercises": [
+                {{
+                    "exercise_id": 20,
+                    "name": "Shoulder press",
+                    "sets": 3,
+                    "reps": "10-12",
+                    "suggested_weight": "40kg-45kg",
+                    "notes": "Do a couple of warm up sets with light weight if needed"
+                }}
+                ]
+            }}
+            ]
+        }}
+        ]
+    }}
+
+    """
